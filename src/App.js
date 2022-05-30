09a51a416cce12d66a068f7a78cb28e6343d21b8
Lewis Pakoti <lewisnpakoti@gmail.com>
2022-05-30 22:24:00 +1200

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  // variables
+  const name = 'John'
+
+  // ternary expressions
+  const isNameShowing = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <h1>Hello, {isNameShowing ? name : 'someone else'}!</h1>
     </div>
   );
 }
