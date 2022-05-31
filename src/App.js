149593a21bacda7d31dd060487b2c8a261c7b176
Lewis Pakoti <lewisnpakoti@gmail.com>
2022-05-31 23:48:19 +1200

    added dynamic search functionality and enabled on-screen rendering for results

--- src/App.js
+++ src/App.js
@@ -22,10 +22,13 @@ const movie1 = {
 
 const App = () => {
 
-  // to get movies from console.log to component - setMovie setter function
+  // To get movies from console.log to component - setMovie setter function
   const [ movies, setMovies] = useState([]);
 
-  // function to fetch movies
+  // Add search functionality
+  const [ searchTerm, setSearchTerm ] = useState('');
+
+  // Function to fetch movies
   const searchMovies = async (title) => {
     // call API
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -45,31 +48,31 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search movies..."
-          value="Superman"
-          onChange={() => {}}>
+          // Dynamically search for movies  instead of having a hardcoded inoput
+          value={searchTerm}
+          // Can now change input field search term
+          onChange={(e) => setSearchTerm(e.target.value)}>
         </input>
         <img
           src={SearchIcon}
           alt="search"
-          onCLick={() => {}}>
+          // Use searchTerm to re-render array of movies via img onClick listener
+          onClick={() => searchMovies(searchTerm)}>
         </img>
       </div>
 
-      {
-        movies?.length > 0
-          ? (
-            <div className="container">
-              {movies.map((movie) => (
-                <MovieCard movie={movie}/>
-              ))}
-            </div>
-          ) : (
-            <div className="empty">
-              <h1>No movies found</h1>
-            </div>
-          )
-
-      }
+      {movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie}/>
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h1>No movies found</h1>
+          </div>
+        )}
     </div>
   );
 }
