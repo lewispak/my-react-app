5635d34d4d90053ea3c0e3b2229c39595c78e1c3
Lewis Pakoti <lewisnpakoti@gmail.com>
2022-05-31 22:37:17 +1200

    added first movie card with static input. Dynamic to come

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import SearchIcon from './search.svg'
 // API URL variable
 const API_URL = 'http://www.omdbapi.com?apikey=9e1aa0a'
 
+const movie1 = {
+  "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
+  "Title": "Italian Spiderman",
+  "Type": "movie",
+  "Year": "2007",
+  "imdbID": "tt2705436"
+}
+
 const App = () => {
 
   // function to fetch movies
@@ -16,7 +24,7 @@ const App = () => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data)
+    console.log(data.Search)
   }
 
   useEffect (() => {
@@ -24,8 +32,41 @@ const App = () => {
   }, []);
 
   return (
-    <h1>This is an app :)</h1>
-  );
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search movies..."
+          value="Superman"
+          onChange={() => {}}>
+        </input>
+        <img
+          src={SearchIcon}
+          alt="search"
+          onCLick={() => {}}>
+        </img>
+      </div>
+
+      <div className="container">
+        <div className="movie">
+
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+          <div>
+            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}></img>
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+   );
 }
 
 export default App;
